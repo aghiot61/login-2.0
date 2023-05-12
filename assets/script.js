9375178ba1d445dd371197f2b2a7daffa63fe16a
Thiago <thiagoschaves61@gmail.com>
2023-05-11 22:03:39 -0300

    login-2.0

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+/*Efeito de passagem*/
+
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
